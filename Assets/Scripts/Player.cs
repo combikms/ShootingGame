1f50ac3fc08b927b123aa748735e3538ae097c37
kang <combikms@naver.com>
2025-08-07 00:06:36 +0900

    :sparkles: Feat: 마우스조작, 미사일발사

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,25 +1,53 @@
+using System;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed;
+
+    [SerializeField]
+    private GameObject weapon;
+
+    [SerializeField]
+    private Transform shootTransform;
+
+    [SerializeField]
+    private float shootInterval = 0.1f;
+    private float lastShotTiem = 0f;
+
+    void Shoot()
+    {
+        if (Time.time - lastShotTiem > shootInterval)
+        {
+            Instantiate(weapon, shootTransform.position, Quaternion.identity);
+            lastShotTiem = Time.time;
+        }
+    }
+
     void Update()
     {
-        // <방법 1>
+        // <키보드1>
         // float horizontalInput = Input.GetAxisRaw("Horizontal");
         // Vector3 moveTo = new Vector3(horizontalInput, 0f, 0f);
         // transform.position += moveTo * moveSpeed * Time.deltaTime;
 
-        // <방법 2>
-        Vector3 moveTo = new Vector3(moveSpeed * Time.deltaTime, 0, 0);
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position -= moveTo;
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.position += moveTo;
-        }
+        // <키보드2>
+        // Vector3 moveTo = new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        // if (Input.GetKey(KeyCode.LeftArrow))
+        // {
+        //     transform.position -= moveTo;
+        // }
+        // else if (Input.GetKey(KeyCode.RightArrow))
+        // {
+        //     transform.position += moveTo;
+        // }
+
+        // <마우스>
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float toX = Mathf.Clamp(mousePos.x, -2.35f, 2.35f);
+        transform.position = new Vector3(toX, transform.position.y, transform.position.z);
+
+        Shoot();
     }
 }
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 10;
+
+    void Start()
+    {
+        Destroy(gameObject, 1f);
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+    }
+}
